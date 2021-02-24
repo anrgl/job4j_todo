9b94601abceb1f8a800475a21a0e17ce95f8579c
Aynur Galimov <galimovaynur@protonmail.com>
2021-02-24 12:07:05 +0500

    3. Лямбды и шаблон wrapper. [#450414]

--- src/main/java/ru/job4j/todo/repository/ItemHibernate.java
+++ src/main/java/ru/job4j/todo/repository/ItemHibernate.java
@@ -2,13 +2,14 @@ package ru.job4j.todo.repository;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import ru.job4j.todo.model.Item;
 
-import java.util.Collection;
 import java.util.List;
+import java.util.function.Function;
 
 public class ItemHibernate {
     private final StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
@@ -16,33 +17,34 @@ public class ItemHibernate {
     private final SessionFactory sf = new MetadataSources(registry)
             .buildMetadata().buildSessionFactory();
 
-    public void add(Item item) {
-        try (Session session = sf.openSession()) {
-            session.beginTransaction();
-            session.save(item);
-            session.getTransaction().commit();
+    private <T> T tx(final Function<Session, T> command) {
+        final Session session = sf.openSession();
+        final Transaction tx = session.beginTransaction();
+        try {
+            T rsl = command.apply(session);
+            tx.commit();
+            return rsl;
+        } catch (final Exception e) {
+            session.getTransaction().rollback();
+            throw e;
+        } finally {
+            session.close();
         }
+    }
+
+    public void add(Item item) {
+        this.tx(session -> session.save(item));
     }
 
     public List<Item> findAll() {
-        List<Item> items;
-        try (Session session = sf.openSession()) {
-            session.beginTransaction();
-            items = session.createQuery("from ru.job4j.todo.model.Item", Item.class)
-                    .list();
-            session.getTransaction().commit();
-        }
-        return items;
+        return this.tx(
+                session -> session.createQuery("from ru.job4j.todo.model.Item", Item.class)
+                        .list());
     }
 
     public List<Item> findCompleteTasks() {
-        List<Item> items;
-        try (Session session = sf.openSession()) {
-            session.beginTransaction();
-            items = session.createQuery("from ru.job4j.todo.model.Item where done = false", Item.class)
-                    .list();
-            session.getTransaction().commit();
-        }
-        return items;
+        return this.tx(
+                session -> session.createQuery("from ru.job4j.todo.model.Item where done = false", Item.class)
+                        .list());
     }
 }
